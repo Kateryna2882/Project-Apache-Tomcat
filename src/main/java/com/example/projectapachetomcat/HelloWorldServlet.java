cbd6baf7d2fd134d7b67efd5a710932b1e648c88
Kateryna2882 <govoryhina82@gmail.com>
2024-02-09 20:42:28 +0200

    Add web.xml

--- src/main/java/com/example/projectapachetomcat/HelloWorldServlet.java
+++ src/main/java/com/example/projectapachetomcat/HelloWorldServlet.java
@@ -1,4 +1,21 @@
 package com.example.projectapachetomcat;
 
-public class HelloWorldServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Hello World Servlet</title></head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
 }
