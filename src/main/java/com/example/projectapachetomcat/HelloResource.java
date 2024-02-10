3821462f1cd0e016560643c4a361c77969accd9a
Kateryna2882 <govoryhina82@gmail.com>
2024-02-10 19:30:44 +0200

    Add

--- src/main/java/com/example/projectapachetomcat/HelloResource.java
+++ src/main/java/com/example/projectapachetomcat/HelloResource.java
@@ -3,12 +3,15 @@ package com.example.projectapachetomcat;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hello() {
+        String message = "Hello, World!";
+        return Response.ok().entity(message).build();
     }
 }
--- src/main/java/com/example/projectapachetomcat/HelloWorldServlet.java
+++ src/main/java/com/example/projectapachetomcat/HelloWorldServlet.java
@@ -1,21 +1,22 @@
 package com.example.projectapachetomcat;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+
         PrintWriter out = response.getWriter();
-        out.println("<html><head><title>Hello World Servlet</title></head>");
-        out.println("<body>");
-        out.println("<h1>Hello, World!</h1>");
-        out.println("</body></html>");
+        out.println("Hello, World!");
     }
 }
